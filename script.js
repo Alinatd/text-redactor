415206f9f1cc1f49044cc41176e957105d731219
Alina Turdakunova <141915076+Alinatd@users.noreply.github.com>
2023-09-13 01:20:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const redactor = () =>{
+   let text = document.getElementById('area').value
+   document.getElementById('view').innerHTML = text
+
+}
+
+//     let elem = document.getElementById('names')
+// const test =() =>{
+// elem.innerHTML = "Hello"
+// // document.body.style.backgroundColor ="blue"
+// document.body.style.backgroundImage = "url(https://guu.ru/wp-content/uploads/%D0%9F%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D0%BD%D0%B8%D0%BC%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%BB%D0%B0%D1%81%D1%81.jpg)"
+// }
